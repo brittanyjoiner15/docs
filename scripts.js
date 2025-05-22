7d4d11da825cee9f61683117be1306a22fe5ef91
Brittany Joiner <brittanyjoiner15@gmail.com>
2025-05-22 16:35:24 -0500

    Documentation edits made through Mintlify web editor

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+console.log("test this runs")
+
+<script type="text/javascript">
+  console.log("test this runs inside script tags")
+</script>
+
 <!-- Start of HubSpot Embed Code -->
   <script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/47598997.js"></script>
 <!-- End of HubSpot Embed Code -->
